3df6f4ee34c79044f5355f7da3d366aae606d747
Yassir Awad <yassir.awad@dce.sa>
2019-07-22 16:43:30 +0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # Digitalcloud SMS Notifier module
+
+## Features
+* Supports multiple SMS providers (twilio and unifonic).
+* A channel for sending SMS notifications.
+* Log the response of the provider for each request.
+* Unified response for all providers.
+
+
+##Installation
+Require the `digitalcloud/sms` package in your `composer.json` and update your dependencies:
+```sh
+    "require": {
+        "digitalcloud/sms": "dev-master"
+    }
+    "repositories": [
+        {
+          "type": "vcs",
+          "url": "https://github.com/digitalCloud/sms"
+        }
+    ]
+```
+
+
+after installing the package you should migrate the tables using this command:
+ ```sh
+ $ php artisan migrate
+ ```
+ 
+finally, publish the config using this command:
+  ```sh
+  $ php artisan vendor:publish --provider="Digitalcloud\SMS\ServiceProvider"
+  ```
+ 
+## Usage
+* Each model that use sms notifications should use `Digitalcloud\SMS\HasSmsLogs` trait
+to allow logging.
+* in your notification class include the SMS channel in via function,
+also `getMobile` and `getMessage` functions.
+```php
+<?php
+
+use Digitalcloud\SMS\SMSChannel;
+
+class YourNotificationClass extends Notification {
+    
+    public function via()
+    {
+        return [SMSChannel::class];
+    }
+    
+    public function getMobile(){
+        return '970567940999';
+    }
+    
+    public function getMessage(){
+        return 'Your activation code is 6523';
+    }
+}
+```
--- src/SMSChannel.php
+++ src/SMSChannel.php
@@ -8,31 +8,29 @@ class SMSChannel
 {
     public function send($notifiable, Notification $notification)
     {
-        $this->notify($notifiable, $notification, $this->getData($notifiable, $notification));
+        $this->validate($notification);
+
+        $this->notify($notifiable, $notification);
     }
 
-    protected function getData($notifiable, Notification $notification)
+    protected function validate(Notification $notification)
     {
-        if (method_exists($notification, 'toSMS')) {
-            return $notification->toSMS($notifiable);
+        if (!method_exists($notification, 'getMobile') || !$notification->getMobile()) {
+            throw new \RuntimeException("No mobile number provided");
         }
 
-        if (method_exists($notification, 'toArray')) {
-            return $notification->toArray($notifiable);
+        if (!method_exists($notification, 'getMessage') || !$notification->getMessage()) {
+            throw new \RuntimeException("No message provided");
         }
-
-        throw new \RuntimeException(
-            'Notification is missing toSMS / toArray method.'
-        );
     }
 
-    private function notify($notifiable, $notification, $data)
+    private function notify($notifiable, $notification)
     {
         $sms = resolve(Provider::class);
         /**
          * @var  ProviderResponse $response
          */
-        $response = $sms->notify($data['mobile'], $data['message']);
+        $response = $sms->notify($notification->getMobile(), $notification->getMessage());
 
         $response->log($notifiable, get_class($notification));
 
